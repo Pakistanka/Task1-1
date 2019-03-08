33c6a6fce771dacfb72334e9bbedbebed92c5c25
Anton Fedorov <anton.pzkt@gmail.com>
2019-03-08 19:53:05 +0100

    js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    arrows: false,
+    vertical: true,
+  	dots: true,
+  	slidesToShow: 1,
+  	slidesToScroll: 1,
+    infinite: false,
+  });
+});
