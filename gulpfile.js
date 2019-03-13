5f8e0789f560f648eddb6c5c5aeb22beb1388a4b
Anton Fedorov <anton.pzkt@gmail.com>
2019-03-14 00:51:28 +0100

    gulp-file

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,29 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
  
 sass.compiler = require('node-sass');
 
+gulp.task('stream', function () {
+    return watch('css/**/*.css', { ignoreInitial: false })
+        .pipe(gulp.dest('build'));
+});
 
+gulp.task('callback', function () {
+    return watch('css/**/*.css', function () {
+        gulp.src('css/**/*.css')
+            .pipe(gulp.dest('build'));
+    });
+}); 
  
 gulp.task('sass', function () {
   return gulp.src('./sass/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+            browsers: ['last 6 versions'],
+            cascade: false
+        }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
 });
