b8e549ae7c5bc336168beec71f232d5c29106c96
Anton Fedorov <anton.pzkt@gmail.com>
2019-03-15 00:47:30 +0100

    added 'grid: true' for autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,9 @@ gulp.task('sass', function () {
   return gulp.src('./sass/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
-            browsers: ['last 6 versions'],
-            cascade: false
+      grid: true,
+      browsers: ['last 6 versions'],
+      cascade: false
         }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.stream());
