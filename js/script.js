cca66404edb21d225439c70e0d8067a3ea95509d
Anton Fedorov <anton.pzkt@gmail.com>
2019-03-15 00:48:04 +0100

    small changes

--- js/script.js
+++ js/script.js
@@ -3,17 +3,22 @@ $(document).ready(function(){
     arrows: false,
     vertical: true,
     infinite: false,
-    verticalSwiping: true
+    verticalSwiping: true,
   });
 
-     $('.tooltip').tooltipster({
-   	theme: 'tooltipster-light'
-   });
+  $('.tooltip').tooltipster({
+   	theme: 'tooltipster-light',
+    trigger: 'click'
+  });
+
+});
+
+$(document).ready(function(){
 
   $('.form__text, .form__link').hide();
 
   $('form__button').click(function() {
-  	$('.form__text, .form__link').show();
+    $('.form__text, .form__link').show();
   });
-  
+
 });
